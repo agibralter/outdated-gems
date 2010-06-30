baadcad6329bcaf012494112dd6d17ac6c6ab597
Aaron Gibralter <aaron.gibralter@gmail.com>
2010-06-30 12:55:22 -0400

    Since bundler 0.9.x and 1.0.x don't have the same Gemfile.lock format I decided to take a different (and better) approach. outdated_gems now uses the `bundle show` output, which is compatible with both 0.9.x and 1.0.x.

--- lib/outdated-gems.rb
+++ lib/outdated-gems.rb
@@ -1,29 +1,26 @@
-require 'yaml'
-
-file = File.join(Dir.pwd, 'Gemfile.lock')
-
-if File.exists?(file)
-  gemfile = File.open(file) { |yf| YAML::load(yf) }
-  gems = gemfile['specs'].inject({}) do |gems, g|
-    g = g.to_a[0]
-    name = g[0]
-    version = g[1]['version']
-    gems[name] = version
-    gems
+if (bundle = `bundle show`) && !bundle.include?("Could not locate Gemfile")
+  gems = {}
+  bundle.each_line do |line|
+    if line =~ /^\s+\* (.*?) \(([\w.]+)\)/
+      gems[$~[1]] = $~[2]
+    end
   end
-
   outdated_gems = {}
-
   gem_list = Gem::SpecFetcher.new.list.values.first
-
   gems.each do |name, v_current|
     latest_gem = gem_list.detect { |g| g.first == name }
-    if latest_gem[1] > Gem::Version.create(v_current)
+    if latest_gem[2] == "ruby" && latest_gem[1] > Gem::Version.create(v_current)
       outdated_gems[name] = "#{v_current} => #{latest_gem[1].version}"
     end
   end
-
-  outdated_gems.each do |name, change|
-    puts "#{name}: #{change}"
+  if outdated_gems.empty?
+    puts "No outdated gems."
+  else
+    puts "Outdated gems:"
+    outdated_gems.each do |name, change|
+      puts "#{name}: #{change}"
+    end
   end
+else
+  puts bundle
 end
